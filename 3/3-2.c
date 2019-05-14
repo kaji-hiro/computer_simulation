a83240fa0f9bd9843b3f903899bb31ecd4386b53
kajihiro-meiji <kajihiro-meiji@localhost.localdomain>
2019-05-14 18:40:53 +0900

    commit 3-2

--- 3/3-2.c
+++ 3/3-2.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <math.h>
-#define NUMBER M_PI / 100
 
-double f2(double x1,double x2,double t, double o){
-  return - x2 / 2 - x1 + 10 * sin(o * t);
+double f2(double x1,double x2,double t,double o){
+  return - x2 - x1 + 10 * sin(o * t);
 }
 
 int main(void){
-  double x1 = 0, x2 = 0, t = 0, o = 0.1;
-  int i = 0;
-  while(i < 100000){
-    if (i > 4000)
-      printf(" %f, %f, %f\n", t, log10(o), x2);
-    x1 += x2 * NUMBER;
-    x2 += NUMBER * f2(x1, x2, t, o);
-    t += NUMBER;
-    i++;
-    o+= 0.01;
+  double x1 = 0, x2 = 0, t = 0, num, max;
+  int i = 0, j = 0;
+  for (double o = 0.1; o <= 10; o+=0.01) {
+    num = M_PI / 100 / o;
+    i = 0;
+    max = 0;
+    while(i < 5001){
+      x1 += x2 * num;
+      x2 += num * f2(x1, x2, t, o);
+      t += num;
+      i++;
+      if (i > 4000){
+        if (max < x2) max = x2;
+      }
+    }
+    j++;
+    printf("%f,%f\n", log10(o), max);
   }
+  return 0;
 }
